4744b57470aeeb0d2ab3bb9f58629f8a72c58062
Juliet <agutu@gmail.com>
2022-02-20 01:09:11 +0300

    add function for save user in the user.py file

--- user.py
+++ user.py
@@ -1,3 +1,6 @@
+import email
+
+
 class user:
     '''
     Class that generates new instances of user
@@ -5,8 +8,20 @@ class user:
 
     user_list = [] #empty list
 
-    def __init__(self,user_name,password):
+    def __init__(self,user_name,password,email):
                 
         self.user_name = user_name
         self.password = password
+        self.email = email
+
+
+    user_list = [] # empty list
+    #Init method up here
+    def save_user(self):
+        '''
+        save_user method saves user objects in user list
+        '''
+
+        user.user_list.append(self)
+    
 
