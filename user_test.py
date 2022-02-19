a1ace2a862d50af3f2e594cb351b8cc89422a304
Juliet <agutu@gmail.com>
2022-02-19 13:33:41 +0300

    create user_test file and import unit test

---
+++ user_test.py
@@ -0,0 +1,11 @@
+import unittest #importing the unitest module
+from user import user #importing user class
+
+class Testuser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: class that helps in creating test cases
+    '''    
