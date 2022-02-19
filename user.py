0739d50817d23a3d844e0e44b7f25c61e79af4ed
Juliet <agutu@gmail.com>
2022-02-19 09:33:58 +0300

    create class variables in user.py file

---
+++ user.py
@@ -0,0 +1,12 @@
+class user:
+    '''
+    Class that generates new instances of user
+    '''
+
+    user_list = [] #empty list
+
+    def __init__(self,user_name,password):
+                
+        self.user_name = user_name
+        self.password = password
+
