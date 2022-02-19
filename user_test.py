634b8df8f6417f6b1d0db0e1ff376c8ad868f28c
Juliet <agutu@gmail.com>
2022-02-19 19:22:19 +0300

    install pyperclip and run a test

--- user_test.py
+++ user_test.py
@@ -1,28 +1,44 @@
-import unittest #importing the unitest module
-from user import user #importing user class
 
-class Testuser(unittest.TestCase):
+import pyperclip #importing pyperclip
+from user import user # importing user class
+  
 
+def test_copy_name(self):  
     '''
-    Test class that defines test cases for the contact class behaviours.
+    Test to confirm that we are copying the name from a found user
+    ''' 
+    self.new_user.save_user()
+    user.copy_name("Juliet")
 
-    Args:
-        unittest.TestCase: class that helps in creating test cases
-    '''  
+    self.assertEqual(self.new_user.name,pyperclip.paste()) 
 
-    def setUp(self):    
-        '''
-        Set up method to run before each test cases.
-        '''
-        self.new_user = user("Juliet","Igotthis*") #create contact object
 
-    def test_init(self):
-        '''
-        test_init test case to test if the object is initialized properly
-        '''
 
-        self.assertEqual(self.new_user.user_name,"Juliet")  
-        self.assertEqual(self.new_user.password,"Igotthis*") 
+# import unittest #importing the unitest module
+# from user import user #importing user class
 
-if __name__ == '__main__':
-    unittest.main()
+# class Testuser(unittest.TestCase):
+
+#     '''
+#     Test class that defines test cases for the user class behaviours.
+
+#     Args:
+#         unittest.TestCase: class that helps in creating test cases
+#     '''  
+
+#     def setUp(self):    
+#         '''
+#         Set up method to run before each test cases.
+#         '''
+#         self.new_user = user("Juliet","Igotthis*") #create contact object
+
+#     def test_init(self):
+#         '''
+#         test_init test case to test if the object is initialized properly
+#         '''
+
+#         self.assertEqual(self.new_user.user_name,"Juliet")  
+#         self.assertEqual(self.new_user.password,"Igotthis1*") 
+
+# if __name__ == '__main__':
+#     unittest.main()
